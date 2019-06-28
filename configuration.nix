97803aec90bbef6ecdc99884a32a4f2f853b2619
Zachary Newman <z@znewman.net>
2019-06-28 18:28:52 -0400

    nixos: Add installation script for nixos configuration

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,8 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      # needs to be absolute since we symlink this file in
+      /etc/nixos/hardware-configuration.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
