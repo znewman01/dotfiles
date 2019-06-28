8aa7513e04ae08be6d2beb9f9e9cf94a1d6afc60
Zachary Newman <z@znewman.net>
2019-06-28 19:57:07 -0400

    nixos: Add home-manager

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-ln -s ${PWD}/configuration.nix /etc/nixos/configuration.nix
+if [ ! -f /etc/nixos/configuration.nix ]; then
+  ln -s ${PWD}/configuration.nix /etc/nixos/configuration.nix
+fi
+
+nix-channel --add https://github.com/rycee/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+
+NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
+nix-shell '<home-manager>' -A install
+
+if [ ! -f $HOME/.config/nixpkgs/home.nix ]; then
+  ln -s $PWD/home.nix $HOME/.config/nixpkgs/home.nix
+fi
