24611caf5bbfd7814b1d3fe7ae350a1f6541ac8a
Zachary Newman <z@znewman.net>
2019-07-03 21:29:55 -0400

    nixos: add emacs, spacemacs (though not working yet)

--- home.nix
+++ home.nix
@@ -79,6 +79,21 @@ in
     };
   };
 
+  programs.emacs = {
+    enable = true;
+  };
+
+  home.file.".emacs.d" = {
+    recursive = true;
+    source = pkgs.fetchFromGitHub {
+      owner = "syl20bnr";
+      repo = "spacemacs";
+      # git ls-remote https://github.com/syl20bnr/spacemacs/ master
+      rev = "8c0b8c3440a33df2c291088c47e75fd5e4fbbb61";
+      sha256 = "1af73ls0znbpx73ym9cx22yc6q1dvv375l493ccilg2xpjsgam6w";
+    };
+  };
+
   fonts.fontconfig.enable = true;
 
   systemd.user.services.dropbox = {
