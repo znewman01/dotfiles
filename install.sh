21ed110c2a0984271ce924198aa7a49ac9544eeb
Zachary Newman <z@znewman.net>
2019-07-14 14:27:37 -0400

    install: fix dropbox exclude files
    turns out you need absolute paths

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ dropbox status
 
 read -p "In another terminal, wait until dropbox has actually synced (dropbox status). Then hit [enter]. "
 
-dropbox exclude add "Camera Uploads" Photos Public Saves Screenshots
+dropbox exclude add "$HOME/Dropbox/Camera Uploads" "$HOME/Dropbox/Photos" "$HOME/Dropbox/Public" "$HOME/Dropbox/Saves" "$HOME/Dropbox/Screenshots"
 
 gpg --output "$HOME/Dropbox/keys/${HOSTNAME}.gpg" --export z@znewman.net
 
