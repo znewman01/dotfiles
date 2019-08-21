f284820f5bcb66db37ed040185f8af691e446bdd
Zachary Newman <z@znewman.net>
2019-08-21 15:44:52 -0400

    backups: update backup configuration

--- backup/restic_common.sh
+++ backup/restic_common.sh
@@ -3,11 +3,12 @@ PASS={{PASS_BIN_PATH}}
 BUCKET_NAME=zjn-backup
 BACKUP_SPECS="
 passwords:$HOME/Dropbox/passwords
+keys:$HOME/Dropbox/keys
 org:$HOME/Dropbox/notes
 "
 
 export GOOGLE_PROJECT_ID=999013580212
-export GOOGLE_APPLICATION_CREDENTIALS=$HOME/Dropbox/gcp-service-account.json
+export GOOGLE_APPLICATION_CREDENTIALS=$HOME/Dropbox/keys/gcp-service-account.json
 
 function run_restic {
     "$RESTIC" \
