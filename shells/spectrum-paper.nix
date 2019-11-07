5f838b0d946fdbcb8b4cf84322ad064a56a4e628
Zachary Newman <z@znewman.net>
2019-11-07 12:10:32 -0500

    proj: Improve spectrum-paper requirements

--- shells/spectrum-paper.nix
+++ shells/spectrum-paper.nix
@@ -4,21 +4,12 @@ let
   tex-env = texlive.combine {
     inherit (texlive)
       scheme-medium
-      preprint # for authblk
-      tabu
-      varwidth
-      xargs
-      forloop
-      pbox
-      bigfoot # for suffix
-      environ
-      trimspaces
-      was # for upgreek
       latexmk;
   };
 in pkgs.mkShell rec {
   buildInputs = [
     tex-env
     gnumake
+    tectonic
   ];
 }
