65ea5277127134ffd98d94eab79bb50da0054f9a
Zachary Newman <z@znewman.net>
2019-12-18 10:06:29 -0500

    proj: add more spectrum-impl dependencies
    - Add clippy to spectrum-impl project
    - update rust/cargo version

--- shells/spectrum.nix
+++ shells/spectrum.nix
@@ -1,12 +1,25 @@
 with import <nixpkgs> { };
-
-pkgs.mkShell rec {
-  buildInputs = with pkgs; [
+let src = fetchFromGitHub {
+      owner = "mozilla";
+      repo = "nixpkgs-mozilla";
+      # commit from: 2019-05-15
+      rev = "9f35c4b09fd44a77227e79ff0c1b4b6a69dff533";
+      sha256 = "18h0nvh55b5an4gmlgfbvwbyqj91bklf1zymis6lbdh75571qaz0";
+   };
+in
+with import "${src.out}/rust-overlay.nix" pkgs pkgs;
+stdenv.mkDerivation {
+  name = "rust-env";
+  buildInputs = [
     gmp6
     stdenv
-    cargo
-    rustc
+    latest.rustChannels.stable.rust
     rustfmt
+    rustPackages.clippy
+    protobuf
     gnum4
   ];
+
+  PROTOC = "${pkgs.protobuf}/bin/protoc";
+
 }
