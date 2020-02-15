3af07f2c40c41536bde749dd7141301ac519b383
Zachary Newman <z@znewman.net>
2020-02-15 15:09:40 -0500

    [proj] update spectrum paper dependencies

--- shells/spectrum-paper.nix
+++ shells/spectrum-paper.nix
@@ -4,6 +4,35 @@ let
   tex-env = texlive.combine {
     inherit (texlive)
       scheme-medium
+      blindtext
+      cleveref
+      xifthen
+      ifmtarg
+      preprint  # for authblk
+      mwe
+      todonotes
+      enumitem
+      soul
+      booktabs
+      threeparttable
+      fnpct
+      translations
+      tabu
+      IEEEtran
+      varwidth
+      tcolorbox
+      environ
+      etoolbox
+      trimspaces
+      xargs
+      forloop
+      bigfoot
+      breakurl
+      was
+      sttools
+      pbox
+      ifoddpage
+      relsize
       latexmk;
   };
 in pkgs.mkShell rec {
