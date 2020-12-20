431e491aa9c75c5d8d986921e1a270726ac6bd59
Zachary Newman <z@znewman.net>
2020-12-20 14:05:19 -0500

    [desktop] fix monitor hotplug issue

--- machines/zjn-x1/system.nix
+++ machines/zjn-x1/system.nix
@@ -5,7 +5,15 @@
     sof-firmware
   ];
   hardware.pulseaudio.extraConfig = ''
-      load-module module-alsa-sink device=;w:0,0 channels=4
-      load-module module-alsa-source device=hw:0,6 channels=4
+    load-module module-alsa-sink device=;w:0,0 channels=4
+    load-module module-alsa-source device=hw:0,6 channels=4
+  '';
+  services.udev.path = with pkgs; [lshw];
+  services.udev.extraRules = ''
+    ACTION=="change" \
+    , ATTRS{vendor}=="0x8086" \
+    , ATTRS{subsystem_vendor}=="0x17aa" \
+    , ATTRS{subsystem_device}=="0x2292" \
+    , RUN+="${pkgs.lshw}/bin/lshw"
   '';
 }
