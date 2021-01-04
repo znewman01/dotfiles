4069d5d7e0361c0e6c0f1c9cb82c1e01a4bcbced
Zachary Newman <z@znewman.net>
2021-01-04 14:57:26 -0500

    [emacs] org agenda improvements

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -55,8 +55,7 @@
    ; performance
    (setq org-agenda-dim-blocked-tasks nil
          org-agenda-inhibit-startup t
-         org-agenda-ignore-properties '(effort appt stat category)
-         org-agenda-use-tag-inheritance '(search timeline agenda))
+         org-agenda-ignore-properties '(effort appt stat category))
   (setq org-archive-location "archive/%s::")
   (setq org-default-notes-file (org-file "gtd.org"))
   (setq org-log-done t)
@@ -118,6 +117,7 @@
           subtree-end
         nil)))
 
+  (setq org-startup-indented nil)
   (setq org-stuck-projects '("-SOMEDAY/PROJ" ("NEXT" "BUY") nil ""))
   (setq org-agenda-todo-ignore-scheduled 'past)
   (setq org-agenda-todo-ignore-deadlines 'near)
@@ -126,6 +126,7 @@
         `(
           ("n" "Default View"  ; a little faster
            ((agenda "" ((org-agenda-span 'day) (org-agenda-start-day "+0d")))
+            (stuck "")
             (tags-todo "-personal-SOMEDAY-yak-@errand/NEXT-BUY"
                   ((org-agenda-skip-function 'zjn-org-skip-subtree-if-bad-time)
                    (org-agenda-overriding-header "Work:")))
@@ -153,7 +154,47 @@
             (tags-todo "yak-SOMEDAY/NEXT|BUY"
                   ((org-agenda-overriding-header "Yak shaving:")))))
            ))
+
+  ; https://lists.gnu.org/archive/html/emacs-orgmode/2015-06/msg00266.html
+  (defun org-agenda-delete-empty-blocks ()
+    "Remove empty agenda blocks.
+  A block is identified as empty if there are fewer than 2
+  non-empty lines in the block (excluding the line with
+  `org-agenda-block-separator' characters)."
+    (when org-agenda-compact-blocks
+      (user-error "Cannot delete empty compact blocks"))
+    (setq buffer-read-only nil)
+    (save-excursion
+      (goto-char (point-min))
+      (let* ((blank-line-re "^\\s-*$")
+             (content-line-count (if (looking-at-p blank-line-re) 0 1))
+             (start-pos (point))
+             (block-re (format "%c\\{10,\\}" org-agenda-block-separator)))
+        (while (and (not (eobp)) (forward-line))
+          (cond
+           ((looking-at-p block-re)
+            (when (< content-line-count 2)
+              (delete-region start-pos (1+ (point-at-bol))))
+            (setq start-pos (point))
+            (forward-line)
+            (setq content-line-count (if (looking-at-p blank-line-re) 0 1)))
+           ((not (looking-at-p blank-line-re))
+            (setq content-line-count (1+ content-line-count)))))
+        (when (< content-line-count 2)
+          (delete-region start-pos (point-max)))
+        (goto-char (point-min))
+        ;; The above strategy can leave a separator line at the beginning
+        ;; of the buffer.
+        (when (looking-at-p block-re)
+          (delete-region (point) (1+ (point-at-eol))))))
+    (setq buffer-read-only t))
+  (add-hook 'org-agenda-finalize-hook #'org-agenda-delete-empty-blocks)
+
   (map! :leader "a" (cmd! (org-agenda nil "n")))
+
+  (map! :mode org-mode :n "t" #'org-todo)
+  (map! :map org-agenda-mode-map :localleader "." #'counsel-org-goto-all
+                                 :localleader "/" #'counsel-org-goto-all)
 
   (setq org-show-context-detail
         (quote
