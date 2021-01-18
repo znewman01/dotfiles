8060cb3cdfffd1658b39d6c91bcadffc88baf1a7
Zachary Newman <z@znewman.net>
2021-01-18 15:11:50 -0500

    [life] fix beeminder lichess script

--- home-common.nix
+++ home-common.nix
@@ -145,26 +145,42 @@ in {
 
       LI_GAMES=$(curl -s https://lichess.org/api/user/znewman01 | jq '.perfs.bullet.games + .perfs.blitz.games')
       echo "Current Lichess # of games: ''${LI_GAMES}"
-       
-      if [ $LI_GAMES -le $BEE_GAMES ]; then
+
+      if [ $LI_GAMES -eq $BEE_GAMES ]; then
           echo "Beeminder is up-to-date; exiting."
           exit 0
       fi
 
       echo "Posting the new data point..."
       # TODO: get the response
-      curl -s \
+      DATAPOINT_ID=$(curl -s -X POST \
           --data "''${BEE_AUTH}&value=''${LI_GAMES}" \
-          "''${BEE}/users/znewman01/goals/''${GOAL}/datapoints.json"
+          "''${BEE}/users/znewman01/goals/''${GOAL}/datapoints.json" \
+          | jq '.id' | sed 's/"//g')
 
-      # TODO: all the below
-      # Get the goal
-      # If baremin >= 0: exit
-      # total_charge = -baremin
-      # check comment from prior data point ($TMP_FILE)
-      # if it has a # (charged), to_charge = total_charge - previous_charged
-      # charge!
-      # update most recent data point to include the total_charge today
+      BAREMIN=$(curl -s "''${BEE}/users/znewman01/goals/lichess-fast?''${BEE_AUTH}&count=1" | jq '.baremin' | sed 's/["+]//g')
+      if [ $BAREMIN -ge 0 ]; then
+          echo "Didn't derail!"
+          exit
+      fi
+
+      TOTAL_CHARGE=$(expr 0 - $BAREMIN || true)
+      echo "Total to charge: $TOTAL_CHARGE"
+
+      ALREADY_CHARGED=$(jq 'try (first.comment | tonumber) catch 0' $TMP_FILE | sed 's/"//g')
+      echo "Already charged today: ''${ALREADY_CHARGED}"
+
+      TO_CHARGE=$(expr $TOTAL_CHARGE - $ALREADY_CHARGED || true)
+      echo "Need to charge: ''${TO_CHARGE}"
+
+      if [ $TO_CHARGE -gt 0 ]; then
+          echo "Charging..."
+          curl -s -X POST "''${BEE}/charges.json?''${BEE_AUTH}&dryrun=true&amount=''${TO_CHARGE}&note=lichess-fast"
+      else
+          echo "Not charging."
+      fi
+      echo curl -X PUT "''${BEE}/users/znewman01/goals/lichess-fast/datapoints/''${DATAPOINT_ID}.json?''${BEE_AUTH}&comment=$TOTAL_CHARGE"
+      curl -X PUT "''${BEE}/users/znewman01/goals/lichess-fast/datapoints/''${DATAPOINT_ID}.json?''${BEE_AUTH}&comment=$TOTAL_CHARGE"
     '';
     executable = true;
   };
