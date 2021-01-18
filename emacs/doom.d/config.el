5e17fdb9d2c5570d01575dae5e201e0d0e1f2ab0
Zachary Newman <z@znewman.net>
2021-01-18 15:13:19 -0500

    [emacs] improve Rust settings
    - better Cargo keybindings
    - keybindings work in TOML mode

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -824,7 +824,24 @@
 (setq tramp-inline-compress-start-size 1000000)
 
 (after! rustic
-  (setq rustic-lsp-server 'rust-analyzer))
+  (setq rustic-lsp-server 'rust-analyzer)
+
+  (map! :map (conf-toml-mode-map rustic-mode-map)
+        :localleader
+        (:prefix ("c" . "cargo")
+          :desc "cargo audit"    "a" #'+rust/cargo-audit
+          :desc "cargo build"    "b" #'rustic-cargo-build
+          :desc "cargo bench"    "B" #'rustic-cargo-bench
+          :desc "cargo check"    "c" #'rustic-cargo-check
+          :desc "cargo clippy"   "C" #'rustic-cargo-clippy
+          :desc "cargo doc"      "d" #'rustic-cargo-doc
+          :desc "cargo fmt"      "f" #'rustic-cargo-fmt
+          :desc "cargo new"      "n" #'rustic-cargo-new
+          :desc "cargo outdated" "o" #'rustic-cargo-outdated
+          :desc "cargo run"      "r" #'rustic-cargo-run)
+        (:prefix ("t" . "cargo test")
+          :desc "all"          "a" #'rustic-cargo-test
+          :desc "current test" "t" #'rustic-cargo-current-test)))
 
 (after! lsp-mode
   (push "[/\\\\]\\venv$" lsp-file-watch-ignored)
