b93b473e6c3acbdb58e060262649da1024125ca5
Zachary Newman <z@znewman.net>
2021-02-08 09:43:32 -0500

    [meta] tidy

--- machines/zjn-x1/system.nix
+++ machines/zjn-x1/system.nix
@@ -1,16 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    sof-firmware
-  ];
+  environment.systemPackages = with pkgs; [ sof-firmware ];
   hardware.pulseaudio.extraConfig = ''
     load-module module-alsa-sink device=;w:0,0 channels=4
     load-module module-alsa-source device=hw:0,6 channels=4
   '';
 
   # Bizarre hack to allow monitor
-  services.udev.path = with pkgs; [lshw];
+  services.udev.path = with pkgs; [ lshw ];
   services.udev.extraRules = ''
     ACTION=="change" \
     , ATTRS{vendor}=="0x8086" \
