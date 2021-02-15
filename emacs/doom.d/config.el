ea563a7764d9f1bd39a86f9537c261d61a2c22a8
Zachary Newman <z@znewman.net>
2021-02-15 10:47:14 -0500

    [emacs] cleanup

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -875,19 +875,3 @@
   (push "[/\\\\]\\venv$" lsp-file-watch-ignored)
   (push "[/\\\\]\\.venv$" lsp-file-watch-ignored))
 (remove-hook 'doom-first-buffer-hook #'smartparens-global-mode)
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
