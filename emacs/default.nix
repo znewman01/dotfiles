a8c8ee5a33725913a1564e553e416abf73aef647
Zachary Newman <z@znewman.net>
2021-02-16 15:01:33 -0500

    [desktop] emacs default for PDF files

--- emacs/default.nix
+++ emacs/default.nix
@@ -58,6 +58,24 @@ in {
     '';
     executable = true;
   };
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = { "application/pdf" = "emacsclient.desktop"; };
+  };
+  xdg.dataFile."applications/emacsclient.desktop".text = ''
+    [Desktop Entry]
+    Name=EmacsClient
+    GenericName=Text Editor
+    Comment=Edit text
+    MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
+    Exec=em %F
+    Icon=emacs
+    Type=Application
+    Terminal=false
+    Categories=Development;TextEditor;
+    StartupWMClass=Emacs
+    Keywords=Text;Editor;
+  '';
 
   home.file.".emacs.d.template" = {
     source = pkgs.fetchFromGitHub {
