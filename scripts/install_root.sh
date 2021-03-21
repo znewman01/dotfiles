7b5d5f7a02dc9765d127b0c858ad5de48cbe3b12
Zachary Newman <z@znewman.net>
2021-03-21 11:23:22 -0400

    [meta] add template for new hosts

--- scripts/install_root.sh
+++ scripts/install_root.sh
@@ -2,13 +2,24 @@
 set -x
 set -e
 
+# MUST be run from dotfiles repo root
+# TODO: check/enforce
+
 SYSTEM_WIDE_CONFIG="/etc/nixos/configuration.nix"
 
-read -p "Hostname for this machine? " DESIRED_HOSTNAME
-sed -e "s'{{HOSTNAME}}'${DESIRED_HOSTNAME}'g" \
-    configuration.nix.template > configuration.nix
-sed -e "s'{{HOSTNAME}}'${DESIRED_HOSTNAME}'g" \
-    home.nix.template > home.nix
+HOSTID=$(head -c 8 /etc/machine-id)
+STATEVERSION=$(nix eval -f '<nixpkgs/nixos>' 'config.system.stateVersion')
+
+cp -r hosts/machines/template "hosts/${HOSTNAME}"
+
+for template_file in $(find . -name '*.template'); do
+  cat "$template_file" \
+    | sed -e "s'{{HOSTNAME}}'${HOSTNAME}'g" \
+    | sed -e "s'{{HOSTID}}'${HOSTID}'g" \
+    | sed -e "s'{{STATEVERSION}}'${STATEVERSION}'g" \
+    > "${template_file%.template}"
+  # TODO: delete extra files
+done
 
 if [ -f /etc/nixos/configuration.nix ]; then
   mv "$SYSTEM_WIDE_CONFIG" "${SYSTEM_WIDE_CONFIG}.bak"
