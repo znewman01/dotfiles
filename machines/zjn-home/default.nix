8921bf875d4c8ccbcba67ff43cd8848fa70962d9
Zachary Newman <z@znewman.net>
2021-03-24 14:52:53 -0400

    [os] mess with ephemeral root FS
    use impermanence instead of symlinks

--- machines/zjn-home/default.nix
+++ machines/zjn-home/default.nix
@@ -1,14 +1,27 @@
 { config, pkgs, ... }:
 
-{
-  imports = [ ];
+let
+  impermanence = builtins.fetchTarball {
+    url =
+      "https://github.com/nix-community/impermanence/archive/master.tar.gz";
+  };
+in {
+  imports = [ "${impermanence}/home-manager.nix" ];
 
-  systemd.user.tmpfiles.rules = [
-    "L %h/git      - - - - /persist/zjn/git"
-    "L %h/.gnupg   - - - - /persist/zjn/gnupg"
-    "L %h/.ssh     - - - - /persist/zjn/ssh"
-    "L %h/Maildir  - - - - /persist/zjn/Maildir"
-    "L %h/.dropbox-hm - - - - /persist/zjn/dropbox-hm"
-    "L %h/.gist    - - - - /persist/zjn/gist"
-  ];
+  # Things that should get real backups.
+  # Heuristic: are the source of truth and important.
+  # So: keys, cloud-based things where local changes might need to be recovered
+  # (Dropbox/Syncthing/code)
+  home.persistence."/persist/zjn" = {
+    directories = [ "git" ".gnupg" ".dropbox-hm" ".ssh" ".emacs.d" "Sync" ];
+    files = [ ".gist" ];
+    allowOther = true;
+  };
+
+  # Things that are annoying to reconfigure/rebuild/redownload but not a real
+  # problem to lose.
+  home.persistence."/cache/zjn" = {
+    directories = [ ".cache/mu" "Maildir" ".emacs.d.template" ".mozilla" ".config/syncthing" ".local/share/direnv" ];
+    allowOther = true;
+  };
 }
