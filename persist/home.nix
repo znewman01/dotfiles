2a72dd434a43502a14315046e68bb9477c2978a2
Zachary Newman <z@znewman.net>
2021-07-09 10:05:14 -0400

    [meta] pin to a specific impermanence version

--- persist/home.nix
+++ persist/home.nix
@@ -1,10 +1,12 @@
 { config, pkgs, ... }:
 
 let
-  impermanence = builtins.fetchGit {
-    url = "https://github.com/nix-community/impermanence.git";
-    rev = "58558845bc68dcf2bb32caa80564f7fe3f6cbc61";
-    ref = "master";
+  rev = "58558845bc68dcf2bb32caa80564f7fe3f6cbc61";
+  impermanence = builtins.fetchTarball {
+    # git ls-remote https://github.com/nix-community/impermanence master
+    url = "https://github.com/nix-community/impermanence/archive/${rev}.tar.gz";
+    # To update, make all-0 and run nixos-rebuild switch
+    sha256 = "10z3g4knkvq838zbfq71pkfyl8cffrpavna448wf5mjscycp0gnv";
   };
 in {
   imports = [ "${impermanence}/home-manager.nix" ];
