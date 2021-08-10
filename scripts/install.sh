6f3828ca13a37845558b308566284261ced7c436
Zachary Newman <z@znewman.net>
2021-08-10 18:30:24 -0400

    [meta] install script fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
 set -e
 
-gpg --full-generate-key
-ssh-keygen -t ed25519
+if [ ! gpg --list-keys | grep "($HOSTNAME)"; then
+    tmpdir=$(mktemp -d)
+    chmod 0700 $HOME/.gnupg
+    cat > $tmpdir/keydetails <<EOF
+        %echo Generating a basic OpenPGP key
+        Key-Type: RSA
+        Key-Length: 3072
+        Subkey-Type: RSA
+        Subkey-Length: 2048
+        Name-Real: Zachary Newman
+        Name-Comment: $HOSTNAME
+        Name-Email: z@znewman.net
+        Expire-Date: 0
+        %no-ask-passphrase
+        %no-protection
+        %pubring $HOME/.gnupg/pubring.kbx
+        %secring $HOME/.gnupg/trustdb.gpg
+        %commit
+        %echo done
+    EOF
+    gpg2 --verbose --batch --gen-key keydetails
+    echo -e "5\ny\n" |  gpg2 --command-fd 0 --expert --edit-key z@znewman.net trust;
+    # TODO: gpg --edit-key $KEYID # then type "passwd<RET>"
+fi
+
+if [ ! -f "$HOME/.ssh/id_ed25519" ]; then
+    ssh-keygen -t ed25519 -f "$HOME/.ssh/id_ed25519" -N ""
+fi
 
 find "$HOME/.gnupg/" -type f -exec chmod 600 {} \;
 find "$HOME/.gnupg/" -type d -exec chmod 700 {} \;
@@ -18,6 +44,9 @@ FINGERPRINT=$(gpg --list-keys | grep "Key fingerprint" | head -n 1 | cut -d= -f2
 read -p "On a viable machine, run:
     $ gpg --import $HOME/Sync/keys/${HOSTNAME}.gpg
     $ echo '${FINGERPRINT}:6' | gpg --import-ownertrust
-    $ xargs pass init $FINGERPRINT < $HOME/.password-store/.gpg-id
+    $ echo $FINGERPRINT >> $HOME/.password-store/.gpg-id
+    $ while true; xargs pass init < $HOME/.password-store/.gpg-id && break; done
 
 Verify that you can now read passwords on this machine. Then hit [enter]. "
+
+# gpg --list-keys | grep -oP "(?<=fingerprint =) ([A-F0-9]{4}\s*)+" | tr -d ' ' | sed 's/\(.*\)/\1:6/g'
