9a3ec180451879eeed00ed5bcce265b0232a8e31
Zachary Newman <z@znewman.net>
2022-01-19 14:42:33 -0500

    [desktop] use hosts instead of dnsmasq
    plays better with tailscale

--- desktop/work.nix
+++ desktop/work.nix
@@ -2,9 +2,6 @@
 
 let badhosts = /home/zjn/Sync/documents/badhosts.txt;
 in {
-  networking.networkmanager.dns = "dnsmasq";
-  services.dnsmasq.enable = true;
-  services.dnsmasq.servers = [ "8.8.8.8" "8.8.4.4" ];
-  services.dnsmasq.extraConfig = if builtins.pathExists badhosts then
+  networking.extraHosts = if builtins.pathExists badhosts then
     builtins.readFile badhosts else "";
 }
