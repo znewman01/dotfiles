3c6cf927efd3128164f2f0529a97d14d8ca51884
Zachary Newman <z@znewman.net>
2022-02-06 20:25:52 -0500

    [meta] fix hive config

--- ops/hive.nix
+++ ops/hive.nix
@@ -5,18 +5,21 @@
     imports = [ ../machines/zjn-x1prime/system.nix ];
     home-manager.users.zjn.imports = [ ../machines/zjn-x1prime/default.nix ];
     deployment.allowLocalDeployment = true;
+    deployment = { targetUser = "zjn"; };
   };
 
   zjn-home = { config, pkgs, lib, ... }: {
     imports = [ ../machines/zjn-home/system.nix ];
     home-manager.users.zjn.imports = [ ../machines/zjn-home/default.nix ];
     deployment.allowLocalDeployment = true;
+    deployment = { targetUser = "zjn"; };
   };
 
   zjn-work = { config, pkgs, lib, ... }: {
     imports = [ ../machines/zjn-work/system.nix ];
     home-manager.users.zjn.imports = [ ../machines/zjn-work/default.nix ];
     deployment.allowLocalDeployment = true;
+    deployment = { targetUser = "zjn"; };
   };
 
   zjn-cloud = { config, pkgs, lib, ... }: {
