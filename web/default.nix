f062daab8d0dbcd4b2c2d218229d8b9a76f66fe7
Zachary Newman <z@znewman.net>
2022-02-15 07:21:39 -0600

    [desktop] fix tridactyl native messenger

--- web/default.nix
+++ web/default.nix
@@ -4,8 +4,6 @@ let colors = (import ./../desktop/colors) { lib = lib; };
 in {
   programs.firefox = {
     enable = true;
-    package =
-      pkgs.firefox.override { cfg = { enableTridactylNative = true; }; };
     profiles.zjn = {
       name = "Zachary Newman";
       # settings = {
@@ -175,6 +173,15 @@ in {
     }
   '';
   xdg.configFile."tridactyl/tridactylrc".source = ./tridactylrc;
+  home.file.".mozilla/native-messaging-hosts/tridactyl.json".text = ''
+    {
+        "name": "tridactyl",
+        "description": "Tridactyl native command handler",
+        "path": "${pkgs.tridactyl-native}/share/tridactyl/native_main.py",
+        "type": "stdio",
+        "allowed_extensions": [ "tridactyl.vim@cmcaine.co.uk","tridactyl.vim.betas@cmcaine.co.uk", "tridactyl.vim.betas.nonewtab@cmcaine.co.uk" ]
+    }
+  '';
 
   home.packages = [ pkgs.google-chrome pkgs.tridactyl-native ];
 }
