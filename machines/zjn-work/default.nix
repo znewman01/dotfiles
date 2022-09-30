f9277e5bc54f5a0ca952c49e3adb734b51c472c0
Zachary Newman <z@znewman.net>
2022-09-30 17:35:51 -0400

    [meta] update persist

--- machines/zjn-work/default.nix
+++ machines/zjn-work/default.nix
@@ -1,4 +1,4 @@
-{ config, nixpkgs, lib, doom-emacs, ... }:
+{ config, nixpkgs, lib, doom-emacs, impermanence, ... }:
 
 {
   imports = [
@@ -8,7 +8,11 @@
     ../../desktop/work.nix
     ../../work/default.nix
   ];
-  home-manager.users.zjn.imports = [ ./home.nix doom-emacs.hmModule ];
+  home-manager.users.zjn.imports = [
+    ./home.nix
+    doom-emacs.hmModule
+    impermanence.nixosModules.home-manager.impermanence
+  ];
 
   networking.hostName = "zjn-work";
   system.stateVersion = "21.11";
--- persist/home.nix
+++ persist/home.nix
@@ -1,16 +1,6 @@
 { config, pkgs, ... }:
 
-let
-  rev = "58558845bc68dcf2bb32caa80564f7fe3f6cbc61";
-  impermanence = builtins.fetchTarball {
-    # git ls-remote https://github.com/nix-community/impermanence master
-    url = "https://github.com/nix-community/impermanence/archive/${rev}.tar.gz";
-    # To update, make all-0 and run nixos-rebuild switch
-    sha256 = "10z3g4knkvq838zbfq71pkfyl8cffrpavna448wf5mjscycp0gnv";
-  };
-in {
-  imports = [ "${impermanence}/home-manager.nix" ];
-
+{
   # Things that should get real backups.
   # Heuristic: are the source of truth and important.
   # So: keys, cloud-based things where local changes might need to be recovered
