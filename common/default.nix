5c11878204d7d42c772ac1396f0d114fbf5894bc
Zachary Newman <z@znewman.net>
2022-09-30 17:36:13 -0400

    [meta] add nix-community cachix

--- common/default.nix
+++ common/default.nix
@@ -2,5 +2,11 @@
 
 {
   nixpkgs.config.allowUnfree = true;
+  nix.settings = {
+    substituters = [ "https://nix-community.cachix.org" ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   environment.systemPackages = with pkgs; [ git vim wget ];
 }
