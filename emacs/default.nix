be9f3a24c517a4ff345f213bf1cf7633713c9278
Zachary Newman <z@znewman.net>
2022-10-13 19:45:12 -0400

    [emacs] nix-doom-emacs don't recompile

--- emacs/default.nix
+++ emacs/default.nix
@@ -3,12 +3,34 @@
 {
   home.packages = with pkgs; [ emacs-all-the-icons-fonts ispell ];
 
-  programs.doom-emacs = {
+  programs.doom-emacs = rec {
     enable = true;
     doomPrivateDir = (import ./doom.d) {
       inherit lib;
       inherit (pkgs) stdenv emacs coreutils;
     };
+    # Only init/packages so we only rebuild when those change.
+    doomPackageDir = let
+      filteredPath = builtins.path {
+        path = doomPrivateDir;
+        name = "doom-private-dir-filtered";
+        filter = path: type:
+          builtins.elem (baseNameOf path) [ "init.el" "packages.el" ];
+      };
+    in pkgs.linkFarm "doom-packages-dir" [
+      {
+        name = "init.el";
+        path = "${filteredPath}/init.el";
+      }
+      {
+        name = "packages.el";
+        path = "${filteredPath}/packages.el";
+      }
+      {
+        name = "config.el";
+        path = pkgs.emptyFile;
+      }
+    ];
   };
 
   home.file."bin/em" = {
